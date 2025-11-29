b441604200d3ff963722a97d0c4568c86695a8f4
Hakula Chen <i@hakula.xyz>
2025-11-29 22:53:49 +0800

    feat(init): add NixOS 25.05 configuration

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "NixOS configuration for hakula.xyz";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      disko,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        "hakula.xyz" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            disko.nixosModules.disko
+            ./hosts/hakula.xyz
+          ];
+        };
+      };
+    };
+}
