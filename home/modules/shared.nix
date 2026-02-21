7cd5320969ed9a3d8b813514faa7915ab8f37165
Hakula Chen <i@hakula.xyz>
2026-02-22 02:08:22 +0800

    chore(shared): remove Rustup mirror configuration

--- home/modules/shared.nix
+++ home/modules/shared.nix
@@ -159,8 +159,6 @@ in
     # Rust
     RUSTUP_HOME = "$HOME/.rustup";
     CARGO_HOME = "$HOME/.cargo";
-    RUSTUP_UPDATE_ROOT = "https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup";
-    RUSTUP_DIST_SERVER = "https://mirrors.tuna.tsinghua.edu.cn/rustup";
   };
 
   # ----------------------------------------------------------------------------
